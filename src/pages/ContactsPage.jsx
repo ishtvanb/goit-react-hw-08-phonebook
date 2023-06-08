3f3ae2725912e670b7b207865b703eeab9598089
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-08 20:59:41 +0300

    Updating

---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,32 @@
+import Wrapper from '.components/Wrapper/Wrapper';
+import ContactForm from '.components/ContactForm/ContactForm';
+import ContactsList from '.components/ContactsList/ContactsList';
+import Filter from '.components/Filter/Filter';
+import Loader from '.components/Loader';
+import { PrimaryTitle, SecondaryTitle } from '.components/Wrapper/Wrapper.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectError, selectIsLoading } from 'redux/contacts/selectors';
+
+export const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+    
+  return (
+    <Wrapper>
+      <PrimaryTitle>Phonebook</PrimaryTitle>
+      <ContactForm />
+      <SecondaryTitle>Contacts</SecondaryTitle>
+      <Filter />
+      {isLoading && !error && <Loader />}
+      <ContactsList />
+    </Wrapper>
+  );
+};
+
