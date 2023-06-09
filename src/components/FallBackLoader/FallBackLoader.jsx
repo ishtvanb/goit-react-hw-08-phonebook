74b179dc2e2bcd04b4e85c7fef1aae87b61acb27
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-09 11:32:45 +0300

    Update FallBackLoader.jsx

---
+++ src/components/FallBackLoader/FallBackLoader.jsx
@@ -0,0 +1,21 @@
+import { ColorRing } from 'react-loader-spinner';
+import { Wrapper } from './FallBackLoader.styled';
+import Wrapper from 'components/Wrapper/Wrapper';
+const FallBackLoader = () => {
+  return (
+    <Wrapper>
+      <ColorRing
+        height={100}
+        width={100}
+        color="#1976d2"
+        visible={true}
+        ariaLabel="colorRing-loading"
+        secondaryColor="#1976d2"
+        strokeWidth={4}
+        strokeWidthSecondary={4}
+      />
+    </Wrapper>
+  );
+};
+
+export default FallBackLoader;
