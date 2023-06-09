7bbddd62d303b474991a0a62a366e5136682e6db
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-09 11:25:41 +0300

    Update Wrapper.styled.jsx

---
+++ src/components/Wrapper/Wrapper.styled.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const Div = styled.div`
+  width: 300px;
+  margin: 0 auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+export const PrimaryTitle = styled.h1`
+  margin-bottom: 20px;
+  text-align: center;
+  color: darkblue;
+`;
+
+export const SecondaryTitle = styled.h2`
+  margin-bottom: 20px;
+  text-align: center;
+  color: darkblue;
+`;
