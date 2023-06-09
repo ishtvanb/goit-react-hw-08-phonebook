93e89e9451097ccae48c1faaeab4e55681fd6c15
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-09 11:20:04 +0300

    Update ContactsList.styled.jsx

--- src/components/ContactsList/ContactsList.styled.jsx
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -3,7 +3,8 @@ import styled from "@emotion/styled";
 export const ContactItem = styled.li`
   display: flex;
   align-items: center;
-  box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
   border-radius: 10px;
   padding: 10px;
   background-color: lightblue;
