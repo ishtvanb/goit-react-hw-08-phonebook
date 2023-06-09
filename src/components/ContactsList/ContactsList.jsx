64410fd5d0089060cb6ec3103d83a3ef97f37a04
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-09 11:11:30 +0300

    Update ContactsList.jsx

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,50 @@
+import { BsTrash } from 'react-icons/bs';
+import { ContactItem } from './ContactsList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectVisibleContacts } from 'redux/contacts/selectors';
+import { deleteContact } from 'redux/contacts/operations';
+
+const ContactsList = () => {
+  const contacts = useSelector(selectVisibleContacts);
+  const dispatch = useDispatch();
+
+  const handleDelete = id => dispatch(deleteContact(id));
+
+  return (
+    <ul>
+      {contacts?.map(({ name, phone, id }) => {
+        return (
+          <ContactItem key={id}>
+            <span
+              style={{
+                flexGrow: '2',
+              }}
+            >
+              {name}
+            </span>
+          <span
+              style={{
+                marginRight: '10px',
+              }}
+            >
+              {phone}
+            </span>
+
+            <BsTrash
+              aria-label="delete-icon"
+              style={{
+                cursor: 'pointer',
+                color: 'crimson',
+              }}
+              onClick={() => {
+                handleDelete(id);
+              }}
+            />
+          </ContactItem>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactsList;
