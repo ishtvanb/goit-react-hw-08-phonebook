6a2993f3da40ce13bef15f1393410229804bfab8
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-10 11:04:16 +0300

    Updating

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { MainTitle } from 'components/Wrapper/Wrapper.styled';
+import { PrimaryTitle } from 'components/Wrapper/Wrapper.styled';
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
@@ -13,7 +13,7 @@ const HomePage = () => {
         <Wrapper>
           {!isLoggedIn ? (
             <>
-              <MainTitle>Phonebook Web App</MainTitle>
+              <PrimaryTitle>Phonebook Web App</PrimaryTitle>
               <p
                 style={{
                   textAlign: 'center',
