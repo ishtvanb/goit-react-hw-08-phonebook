8ddf05ff1e99ee69bf86cae2260bb355175f0d90
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-10 13:37:38 +0300

    Updating

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -2,6 +2,8 @@ import { Outlet } from 'react-router-dom';
 import AppBar from './AppBar/AppBar';
 import { Suspense } from 'react';
 import FallBackLoader from './FallBackLoader/FallBackLoader';
+import { Image } from 'mui-image';
+import bgImage from './Images/Phonebook.jpg';
 
 const SharedLayout = () => {
   return (
@@ -10,6 +12,17 @@ const SharedLayout = () => {
       <Suspense fallback={<FallBackLoader />}>
         <Outlet />
       </Suspense>
+      <Image
+        objectFit={'cover'}
+        objectPosition={'center'} 
+        component="img"
+        sx={{
+          height: 50,
+          width: 1300,
+        }}
+        alt='Phonebook'
+        src={bgImage}
+      />
     </>
   );
 };
